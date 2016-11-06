8f6eb24dd088e0c0d371693aa3a94920ec34d87d
Justin Coffey <j.coffey@criteo.com>
2016-11-06 16:37:58 +0100

    Better devloop config for finatra example

--- examples/finatra-project/devloop.js
+++ examples/finatra-project/devloop.js
@@ -26,15 +26,34 @@ let sbt = startSbt({
       watch: ['build.sbt']
 })
 
-let sbtAssembly = sbt.run({
-  name: 'assembly',
-  command: 'assembly',
-  watch: ['src/main/scala/**', 'src/main/resources/**', 'src/test/scala/**', 'src/test/resources/**']
-}).dependsOn(webpack, stylus)
+// without a watch, this will only run when build.sbt is updated
+let packageDependencies = sbt.run({
+  name: 'server deps',
+  command: 'assemblyPackageDependency'
+})
+
+// copy over resources to target every time they change
+let publicResources = sbt.run({
+  name: 'public resources',
+  command: 'copyResources',
+  watch: ['src/main/resources/**']
+}).dependsOn(packageDependencies)
+
+let compileServer = sbt.run({
+  name: 'scalac',
+  command: 'compile',
+  watch: ['src/**/*.scala']
+}).dependsOn(packageDependencies)
+
+let testServer = sbt.run({
+  name: 'test server',
+  command: 'test'
+}).dependsOn(compileServer)
 
 let server = runServer({
+  name: 'server',
   httpPort,
-  sh: `java -jar -Dlog.service.output=service.log -Dlog.access.output=access.log target/scala-2.11/finatra-assembly-0.1.0.jar -http.port=:${httpPort} -admin.port=:9990`
-}).dependsOn(sbtAssembly)
+  sh: `java -cp "target/scala-2.11/finatra-assembly-0.1.0-deps.jar${platform == 'win32' ? ';': ':'}target/scala-2.11/classes" com.criteo.finatra.FinatraServerMain -http.port=:${httpPort} -admin.port=:9990`
+}).dependsOn(testServer)
 
-proxy(server, 8080).dependsOn(sbtAssembly)
+proxy(server, 8080).dependsOn(stylus, webpack, publicResources)
